43427623f5e2d286368df98238c93f160ac9883e
jbrayo <jbrayo@up.edu.ph>
2014-05-07 10:32:55 +0800

    commit

--- ShoppingCart2/src/com/orangeandbronze/jbc/shoppingcart/consoleui/Runner.java
+++ ShoppingCart2/src/com/orangeandbronze/jbc/shoppingcart/consoleui/Runner.java
@@ -3,7 +3,6 @@ package com.orangeandbronze.jbc.shoppingcart.consoleui;
 
 public class Runner {
 	public static void main(String[] args) {
-		System.out.println("hello");
+        System.out.println("hello");
 	}
-
 }
